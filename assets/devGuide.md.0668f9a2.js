6be1a96a15f98d7d7f37b67b32f6f4b6ad3840ca
vincent-the-gamer <1127620453@qq.com>
2023-10-22 16:17:42 +0800

    部署: 2023年10月22日---16时17分42秒

---
+++ assets/devGuide.md.0668f9a2.js
@@ -0,0 +1,47 @@
+import{_ as s,o as n,c as e,Q as a}from"./chunks/framework.3f71c860.js";const m=JSON.parse('{"title":"Development Guide","description":"","frontmatter":{"layout":"doc","title":"Development Guide","lastUpdated":true},"headers":[],"relativePath":"devGuide.md","filePath":"devGuide.md"}'),l={name:"devGuide.md"},p=a(`<h1 id="development-guide" tabindex="-1">Development Guide <a class="header-anchor" href="#development-guide" aria-label="Permalink to &quot;Development Guide&quot;">​</a></h1><h2 id="project-structure" tabindex="-1">Project Structure <a class="header-anchor" href="#project-structure" aria-label="Permalink to &quot;Project Structure&quot;">​</a></h2><p><strong>React for example, in other framework it will be similar.</strong></p><p><strong>PS: Some inner files are omitted.</strong></p><h3 id="normal" tabindex="-1">Normal <a class="header-anchor" href="#normal" aria-label="Permalink to &quot;Normal&quot;">​</a></h3><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;">- public</span></span>
+<span class="line"><span style="color:#e1e4e8;">    favicon.ico</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - images</span></span>
+<span class="line"><span style="color:#e1e4e8;">    manifest.json</span></span>
+<span class="line"><span style="color:#e1e4e8;">- src</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - assets</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - background</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - content</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - global</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - hooks</span></span>
+<span class="line"><span style="color:#e1e4e8;">    index.styl</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - options</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - popup</span></span>
+<span class="line"><span style="color:#e1e4e8;">    vite-env.d.ts</span></span>
+<span class="line"><span style="color:#e1e4e8;">copy.js</span></span>
+<span class="line"><span style="color:#e1e4e8;">options.html</span></span>
+<span class="line"><span style="color:#e1e4e8;">popup.html</span></span>
+<span class="line"><span style="color:#e1e4e8;">package.json</span></span>
+<span class="line"><span style="color:#e1e4e8;">tsconfig.json</span></span>
+<span class="line"><span style="color:#e1e4e8;">tsconfig.node.json</span></span>
+<span class="line"><span style="color:#e1e4e8;">vite.config.ts</span></span>
+<span class="line"><span style="color:#e1e4e8;">vite.background.config.ts</span></span>
+<span class="line"><span style="color:#e1e4e8;">vite.content.config.ts</span></span>
+<span class="line"><span style="color:#e1e4e8;">README.md</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">- public</span></span>
+<span class="line"><span style="color:#24292e;">    favicon.ico</span></span>
+<span class="line"><span style="color:#24292e;">    - images</span></span>
+<span class="line"><span style="color:#24292e;">    manifest.json</span></span>
+<span class="line"><span style="color:#24292e;">- src</span></span>
+<span class="line"><span style="color:#24292e;">    - assets</span></span>
+<span class="line"><span style="color:#24292e;">    - background</span></span>
+<span class="line"><span style="color:#24292e;">    - content</span></span>
+<span class="line"><span style="color:#24292e;">    - global</span></span>
+<span class="line"><span style="color:#24292e;">    - hooks</span></span>
+<span class="line"><span style="color:#24292e;">    index.styl</span></span>
+<span class="line"><span style="color:#24292e;">    - options</span></span>
+<span class="line"><span style="color:#24292e;">    - popup</span></span>
+<span class="line"><span style="color:#24292e;">    vite-env.d.ts</span></span>
+<span class="line"><span style="color:#24292e;">copy.js</span></span>
+<span class="line"><span style="color:#24292e;">options.html</span></span>
+<span class="line"><span style="color:#24292e;">popup.html</span></span>
+<span class="line"><span style="color:#24292e;">package.json</span></span>
+<span class="line"><span style="color:#24292e;">tsconfig.json</span></span>
+<span class="line"><span style="color:#24292e;">tsconfig.node.json</span></span>
+<span class="line"><span style="color:#24292e;">vite.config.ts</span></span>
+<span class="line"><span style="color:#24292e;">vite.background.config.ts</span></span>
+<span class="line"><span style="color:#24292e;">vite.content.config.ts</span></span>
+<span class="line"><span style="color:#24292e;">README.md</span></span></code></pre></div><h3 id="lite" tabindex="-1">Lite <a class="header-anchor" href="#lite" aria-label="Permalink to &quot;Lite&quot;">​</a></h3><p>To be continued...</p>`,8),o=[p];function t(c,i,r,d,y,u){return n(),e("div",null,o)}const g=s(l,[["render",t]]);export{m as __pageData,g as default};
---
+++ assets/zh_hans_devGuide.md.1e21241e.js
@@ -0,0 +1,47 @@
+import{_ as s,o as n,c as a,Q as e}from"./chunks/framework.3f71c860.js";const g=JSON.parse('{"title":"开发指南","description":"","frontmatter":{"layout":"doc","title":"开发指南","lastUpdated":true},"headers":[],"relativePath":"zh_hans/devGuide.md","filePath":"zh_hans/devGuide.md"}'),l={name:"zh_hans/devGuide.md"},p=e(`<h1 id="开发指南" tabindex="-1">开发指南 <a class="header-anchor" href="#开发指南" aria-label="Permalink to &quot;开发指南&quot;">​</a></h1><h2 id="项目结构" tabindex="-1">项目结构 <a class="header-anchor" href="#项目结构" aria-label="Permalink to &quot;项目结构&quot;">​</a></h2><p><strong>以React为例，其它框架中的结构类似</strong></p><p><strong>PS: 一些内部文件就省略了</strong></p><h3 id="normal" tabindex="-1">Normal <a class="header-anchor" href="#normal" aria-label="Permalink to &quot;Normal&quot;">​</a></h3><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;">- public</span></span>
+<span class="line"><span style="color:#e1e4e8;">    favicon.ico</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - images</span></span>
+<span class="line"><span style="color:#e1e4e8;">    manifest.json</span></span>
+<span class="line"><span style="color:#e1e4e8;">- src</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - assets</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - background</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - content</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - global</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - hooks</span></span>
+<span class="line"><span style="color:#e1e4e8;">    index.styl</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - options</span></span>
+<span class="line"><span style="color:#e1e4e8;">    - popup</span></span>
+<span class="line"><span style="color:#e1e4e8;">    vite-env.d.ts</span></span>
+<span class="line"><span style="color:#e1e4e8;">copy.js</span></span>
+<span class="line"><span style="color:#e1e4e8;">options.html</span></span>
+<span class="line"><span style="color:#e1e4e8;">popup.html</span></span>
+<span class="line"><span style="color:#e1e4e8;">package.json</span></span>
+<span class="line"><span style="color:#e1e4e8;">tsconfig.json</span></span>
+<span class="line"><span style="color:#e1e4e8;">tsconfig.node.json</span></span>
+<span class="line"><span style="color:#e1e4e8;">vite.config.ts</span></span>
+<span class="line"><span style="color:#e1e4e8;">vite.background.config.ts</span></span>
+<span class="line"><span style="color:#e1e4e8;">vite.content.config.ts</span></span>
+<span class="line"><span style="color:#e1e4e8;">README.md</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">- public</span></span>
+<span class="line"><span style="color:#24292e;">    favicon.ico</span></span>
+<span class="line"><span style="color:#24292e;">    - images</span></span>
+<span class="line"><span style="color:#24292e;">    manifest.json</span></span>
+<span class="line"><span style="color:#24292e;">- src</span></span>
+<span class="line"><span style="color:#24292e;">    - assets</span></span>
+<span class="line"><span style="color:#24292e;">    - background</span></span>
+<span class="line"><span style="color:#24292e;">    - content</span></span>
+<span class="line"><span style="color:#24292e;">    - global</span></span>
+<span class="line"><span style="color:#24292e;">    - hooks</span></span>
+<span class="line"><span style="color:#24292e;">    index.styl</span></span>
+<span class="line"><span style="color:#24292e;">    - options</span></span>
+<span class="line"><span style="color:#24292e;">    - popup</span></span>
+<span class="line"><span style="color:#24292e;">    vite-env.d.ts</span></span>
+<span class="line"><span style="color:#24292e;">copy.js</span></span>
+<span class="line"><span style="color:#24292e;">options.html</span></span>
+<span class="line"><span style="color:#24292e;">popup.html</span></span>
+<span class="line"><span style="color:#24292e;">package.json</span></span>
+<span class="line"><span style="color:#24292e;">tsconfig.json</span></span>
+<span class="line"><span style="color:#24292e;">tsconfig.node.json</span></span>
+<span class="line"><span style="color:#24292e;">vite.config.ts</span></span>
+<span class="line"><span style="color:#24292e;">vite.background.config.ts</span></span>
+<span class="line"><span style="color:#24292e;">vite.content.config.ts</span></span>
+<span class="line"><span style="color:#24292e;">README.md</span></span></code></pre></div><h3 id="lite" tabindex="-1">Lite <a class="header-anchor" href="#lite" aria-label="Permalink to &quot;Lite&quot;">​</a></h3><p>未完待续。。。</p>`,8),o=[p];function c(t,i,r,d,y,h){return n(),a("div",null,o)}const _=s(l,[["render",c]]);export{g as __pageData,_ as default};
